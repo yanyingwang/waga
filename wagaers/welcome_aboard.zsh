8abe22839408e219ea3aa1e386ee4fbe48cf7241
YanyingWang <yanyingwangwang@gmail.com>
2015-03-07 00:40:21 +0800

    ♥

--- wagaers/welcome_aboard.zsh
+++ wagaers/welcome_aboard.zsh
@@ -1,4 +1,4 @@
 welcome_aboard()
 {
-  echo -e "\n \e[31m>> \e[32mWelcome \e[33maboard \e[34m& \e[35mHappy \e[36mhacking\e[35m, \e[95m$(whoami) \e[31m!!! \e[0m \n"
+  echo -e "\n \e[31m>> \e[32mHappy hacking \e[33m$(whoami) \e[34m- \e[35mTerminator \e[36m♥  \e[95myou \e[31m!!! \e[0m \n"
 }
